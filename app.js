82123487274bb4ef507aefc2810ea17a7f0addfa
user <user@MacBook-Pro.local>
2025-03-22 08:07:37 +0100

    render single product

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ window.onload = () =>{
 
 }
 
-
-
 const renderProducts = (products) => {
 
   const wrapper = document.getElementById("wrapper");
@@ -59,6 +57,7 @@ const renderProducts = (products) => {
     button.className = "buy-now";
     button.textContent = "open";
 
+    button.addEventListener('click', ()=> getSingleProduct(product.id));        
 
     card.appendChild(imageWrapper);
     card.appendChild(contentWrapper);
@@ -73,6 +72,74 @@ const renderProducts = (products) => {
     wrapper.append(card);
   }
 }
+
+
+
+
+
+
+const getSingleProduct = async (id) => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  try{
+      const apiResponse = await fetch(url, {
+         "method":"GET",
+         "application-content":"application-json" 
+      })
+      const product = await apiResponse.json();
+      console.log(product);
+      renderSingleproduct(product);
+      // const currentUrl = window.location.href;
+      console.log(currentUrl);            
+  
+  } catch (e){
+      throw new Error();
+  }
+}
+
+
+
+const renderSingleproduct = (product) => {
+  const productItem = document.getElementById("single-product");
+  productItem.style.marginBottom = "200px";
+
+  const fullProdCard = document.createElement("div");
+  fullProdCard.className = "product";
+  const fullProdImageWrapper = document.createElement("div");
+  fullProdImageWrapper.className = "fullProdImageWrapper";
+  const price = document.createElement("div");
+  price.style.fontSize = "30px";
+  price.style.color = "#000";
+
+  const title = document.createElement("h3");
+  const image = document.createElement("img");
+  image.style.height = "700px";
+  const des = document.createElement("p");
+  const ratingBox = document.createElement("div");
+  for(let i = 0; i < product?.rating?.rate; i++){
+      let rating = document.createElement("i");
+      rating.className = "fa fa-star";
+      rating.style.color = "orange";
+      ratingBox.appendChild(rating);
+  }
+
+  des.style.color = "grey";
+  des.textContent = product.description;
+
+  image.src = product.image;
+  price.textContent = `$ ${product?.price}`
+  title.textContent = product.title;
+  title.style.color = "blue";
+
+  fullProdImageWrapper.appendChild(image);
+  fullProdCard.appendChild(fullProdImageWrapper);
+  fullProdCard.appendChild(title);
+  fullProdCard.appendChild(price);
+  fullProdCard.appendChild(des);
+  fullProdCard.appendChild(ratingBox);
+
+  productItem.appendChild(fullProdCard);
+}
+
 
 
 
